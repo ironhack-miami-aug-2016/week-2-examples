c41b1ad62893acc8a56216e1a41c60fe14c4384d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-22 10:15:09 -0400

    Unit Testing: Add optional calculator cases

--- day-1/unit_testing/app.rb
+++ day-1/unit_testing/app.rb
@@ -12,3 +12,27 @@ p the_calculator.add("6") == 6
 p the_calculator.add("3") == 3
 
 p the_calculator.add("") == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# BONUS:
+p the_calculator.add("7$7") == 14
+p the_calculator.add("8$4") == 12
+
+p the_calculator.add("8$4") == 12
+
+p the_calculator.add("7,7,1,2") == 17
+p the_calculator.add("7$7$1$2") == 17
+
+p the_calculator.add("7$7,1,2") == 17
