dbc084a659e1782d483432bcee27644aaf488a19
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-23 14:34:31 -0400

    Sinatra: Add URL parameters example

--- day-2/sinatra/app.rb
+++ day-2/sinatra/app.rb
@@ -38,3 +38,34 @@ get "/pizza" do
   erb :favorite_pizza
 end
 
+
+
+users = [
+  { :username => "starwars",     :name => "Star Wars",     :bio => "The official home of Star Wars on Twitter.",           :avatar => "https://pbs.twimg.com/profile_images/631535425333518336/D-i_GqpT_400x400.jpg" },
+  { :username => "celinedion",   :name => "Celine Dion",   :bio => "Posts signed / Publications signées TC = Team Céline", :avatar => "https://pbs.twimg.com/profile_images/733642354565996544/JrzvO3Y7_400x400.jpg" },
+  { :username => "khalifenizar", :name => "Nizar Khalife", :bio => "I like pizza.",                                        :avatar => "https://pbs.twimg.com/profile_images/480477171362701313/HfqN0EU7_400x400.jpeg" }
+]
+
+
+# http://localhost:4567/users/ironhack
+# http://localhost:4567/users/starwars
+#                             --------
+#                                |
+#               ------------------
+#               |
+get "/users/:username" do
+    #           |
+    #           ------------------
+    #                            |
+  @user_name_string = params[:username]
+
+  @the_user = users.find { |the_user| the_user[:username] == @user_name_string }
+
+  if @the_user == nil
+    erb :no_user
+  else
+    erb :user_profile
+  end
+end
+
+
